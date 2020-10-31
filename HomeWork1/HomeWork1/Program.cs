a183c5ba6a33b3b7a3b86c270fc683bac3481495
iexhollow <asergeevjobfl@gmail.com>
2020-10-31 10:58:57 +0300

    Первое задание

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сергеев Александр
+            // 1) Написать метод, возвращающий минимальное из трёх чисел.
+
+            Console.Write("Введите А ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите А ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите А ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (c < a && c < b)
+            {
+                Console.WriteLine($"min = {c}");
+            }
+            else
+            {
+                if (a < b)
+                {
+                    Console.WriteLine($"min = {a}");
+                }
+                else
+                {
+                    Console.WriteLine($"min = {b}");
+                }
+            }
+        }
+    }
+}
