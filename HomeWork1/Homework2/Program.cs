4d5de837428cb8c1d28a714051663b86f116de5b
iexhollow <asergeevjobfl@gmail.com>
2020-10-31 11:01:01 +0300

    2 Задание

---
+++ HomeWork1/Homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2) Написать метод подсчета количества цифр числа.
+
+            Console.Write("Введите число : ");
+            int num = int.Parse(Console.ReadLine());
+            int i = 0;
+            while (num > 0)
+            {
+                i++;
+                num /= 10;
+            }
+            Console.WriteLine($"Количество цифр введенного числа : {i}");
+            Console.ReadKey();
+        }
+    }
+    
+}
