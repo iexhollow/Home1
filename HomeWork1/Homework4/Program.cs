4b76b2cba5a74ef94873f4c339a64a689cab0443
iexhollow <asergeevjobfl@gmail.com>
2020-10-31 11:05:17 +0300

    4 задание

---
+++ HomeWork1/Homework4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4) Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. На выходе истина, если прошел авторизацию,
+            //и ложь, если не прошел (Логин: root, Password: GeekBrains). Используя метод проверки логина и пароля, написать программу:
+            //пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает.
+            //С помощью цикла do while ограничить ввод пароля тремя попытками.
+
+            string login = "root";
+            string password = "GeekBrains";
+
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Введите логин: ");
+                string checkLogin = Console.ReadLine();
+
+                Console.WriteLine("Введите пароль: ");
+                string checkPassword = Console.ReadLine();
+
+
+                if (login == checkLogin && password == checkPassword)
+                {
+                    Console.WriteLine("Добро пожаловать");
+                    Console.ReadLine();
+                    break;
+                }
+                Console.WriteLine("Неверно введен логин или пароль");
+                Console.ReadLine();
+                ++count;
+            } while (count < 3);
+        }
+    }
+}
